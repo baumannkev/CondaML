4622382b1d2132936c9016e9b36e6e6141cebe37
Kevin Baumann <baumannkev@gmail.com>
2022-01-28 08:54:17 -0800

    Added some functions from Sirine's notebook

--- hvac-ml-app.py
+++ hvac-ml-app.py
@@ -1,10 +1,10 @@
 import streamlit as st
 import pandas as pd
+import matplotlib as plt
 import numpy as np
 import pickle
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import mean_squared_error, mean_absolute_error
-# import matplotlib.pyplot as plt
 future_time = 1
 size_window = 1
 from numpy import mean
@@ -40,12 +40,18 @@ if uploaded_file is not None:
     input_df = pd.read_csv(uploaded_file)
 else:
     def user_input_features():
-        island = st.sidebar.selectbox('Building',('NE01','SE12','SW3'))
-        sex = st.sidebar.selectbox('Room',('412','301'))
-        bill_length_mm = st.sidebar.slider('Parameter 1', 32.1,59.6,43.9)
-        bill_depth_mm = st.sidebar.slider('Parameter 2', 13.1,21.5,17.2)
-        flipper_length_mm = st.sidebar.slider('Parameter 3', 172.0,231.0,201.0)
-        body_mass_g = st.sidebar.slider('Parameter 4', 2700.0,6300.0,4207.0)
+        # island = st.sidebar.selectbox('Building',('NE01','SE12','SW3'))
+        # sex = st.sidebar.selectbox('Room',('412','301'))
+        # bill_length_mm = st.sidebar.slider('Parameter 1', 32.1,59.6,43.9)
+        # bill_depth_mm = st.sidebar.slider('Parameter 2', 13.1,21.5,17.2)
+        # flipper_length_mm = st.sidebar.slider('Parameter 3', 172.0,231.0,201.0)
+        # body_mass_g = st.sidebar.slider('Parameter 4', 2700.0,6300.0,4207.0)
+        island = st.sidebar.selectbox('Island',('Biscoe','Dream','Torgersen'))
+        sex = st.sidebar.selectbox('Sex',('male','female'))
+        bill_length_mm = st.sidebar.slider('Bill length (mm)', 32.1,59.6,43.9)
+        bill_depth_mm = st.sidebar.slider('Bill depth (mm)', 13.1,21.5,17.2)
+        flipper_length_mm = st.sidebar.slider('Flipper length (mm)', 172.0,231.0,201.0)
+        body_mass_g = st.sidebar.slider('Body mass (g)', 2700.0,6300.0,4207.0)
         data = {'island': island,
                 'bill_length_mm': bill_length_mm,
                 'bill_depth_mm': bill_depth_mm,
@@ -94,3 +100,33 @@ st.write(penguins_species[prediction])
 
 st.subheader('Prediction Probability')
 st.write(prediction_proba)
+
+st.write("""
+# BCIT Room Temperature Prediction App
+
+This app predicts the **room temperatures** at BCIT!
+
+Data obtained from Kaizen CopperTreeAnalytics
+""")
+
+df=pd.read_csv("Trend_Logs.csv", parse_dates=True )
+df.columns
+
+df = df.drop(['NE01_AHU7_EFS_POLL_TL','NE01_AHU7_SAT_POLL_TL', 'NE01_AHU7_EF_VFD_AL_COV_TL','NE01_AHU7_BSP_POLL_TL', 'NE01_AHU7_WEST_DSP_POLL_TL',
+'NE01_AHU7_EAST_DSP_POLL_TL'],axis=1)
+
+df['Timestamp']=pd.to_datetime(df['Timestamp'])
+
+df = df.resample('15min', on='Timestamp').mean()
+
+st.write(df['NE01_AHU7_RESET_POLL_TL'].sum())
+
+df.isnull().sum()
+
+df = df.dropna()
+
+df.isnull().sum()
+
+corrmat = df.corr()
+f, ax = plt.subplots(figsize=(12, 9))
+sns.heatmap(corrmat, cbar=True, annot=True, square=True, fmt='.2f')
